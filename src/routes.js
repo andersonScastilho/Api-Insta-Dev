4b70789dfe2ebc2b9d3fea31331b92521f149ddc
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-04-09 16:22:53 -0300

    feat: add schema to auth validation

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,12 @@ const SchemaValidator = require('./apps/middlewares/SchemaValidator');
 
 const UserController = require('./apps/controllers/UserController');
 const UserSchema = require('./schema/Create.User.Schema.json');
+const AuthSchema = require('./schema/Auth.Schema.json');
 const AuthenticationController = require('./apps/controllers/AuthenticationController');
 
 const routes = new Router();
 
 routes.post('/users', SchemaValidator(UserSchema), UserController.store);
-routes.post('/auth', AuthenticationController.store);
+routes.post('/auth', SchemaValidator(AuthSchema), AuthenticationController.store);
 
 module.exports = routes;
