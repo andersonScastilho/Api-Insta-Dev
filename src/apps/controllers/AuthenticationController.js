cd9869efdbac122641a4532896dc45f439d58855
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-04-09 15:54:11 -0300

    feat: use jwt

---
+++ src/apps/controllers/AuthenticationController.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+class AuthenticationController {
+  async store(req, res) {
+    const { email, user_name, password } = req.body;
+
+    const whereClause = {};
+
+    if (email) {
+      whereClause.email = email;
+    } else if (user_name) {
+      whereClause.user_name = user_name;
+    } else {
+      return res.status(401).json({ error: 'we need email or password' });
+    }
+
+    const user = await User.findOne({
+      where: whereClause,
+    });
+
+    if (!user) {
+      return res.statu(404).json({
+        error: 'User not found',
+      });
+    }
+
+    if (!user.checkPassword(password)) {
+      return res.status(401).json({ error: 'Password does not match!' });
+    }
+
+    const { id, user_name: username } = user;
+
+    const token = jwt.sign({}, process.env.HASH_BCRYPT, { expiresIn: '7d' });
+
+    return res.status(200).json({ user: { id, user_name: username }, token });
+  }
+}
+
+module.exports = new AuthenticationController();
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,11 @@ const SchemaValidator = require('./apps/middlewares/SchemaValidator');
 
 const UserController = require('./apps/controllers/UserController');
 const UserSchema = require('./schema/Create.User.Schema.json');
+const AuthenticationController = require('./apps/controllers/AuthenticationController');
 
 const routes = new Router();
 
 routes.post('/users', SchemaValidator(UserSchema), UserController.store);
-
-routes.get('/health', (req, res) => res.send({ message: 'Get status: true' }));
+routes.post('/auth', AuthenticationController.store);
 
 module.exports = routes;
