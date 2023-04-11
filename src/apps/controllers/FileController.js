4a4c3d369ab6fa78a1adbedbbdb28de77e8d8043
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-04-11 20:32:54 -0300

    refatctor: use backblazebucket to store images

--- src/apps/controllers/FileController.js
+++ src/apps/controllers/FileController.js
@@ -1,9 +1,48 @@
+require('dotenv').config();
+
+const fsp = require('fs/promises');
+const B2 = require('backblaze-b2');
+
+const b2 = new B2({
+  applicationKeyId: process.env.BACKBLAZEAPPLICATIONKEYID,
+  applicationKey: process.env.BACKBLAZEAPPLICATIONKEY,
+});
+
+const unlinkAsync = fsp.unlink;
+
 class FileController {
   async upload(req, res) {
-    const { filename } = req.file;
-    return res.status(200).json({
-      url: `uploads/${filename}`,
-    });
+    const { filename, path } = req.file;
+
+    try {
+      const file = await fsp.readFile(`uploads/${filename}`, (err, data) => {
+        if (err) {
+          throw err;
+        }
+        return data;
+      });
+
+      await b2.authorize();
+
+      const { data: { uploadUrl, authorizationToken } } = await b2.getUploadUrl({
+        bucketId: process.env.BACKBLAZEBUCKETID,
+      });
+
+      const { data } = await b2.uploadFile({
+        uploadUrl,
+        uploadAuthToken: authorizationToken,
+        fileName: filename,
+        data: file,
+      });
+
+      await unlinkAsync(path);
+
+      return res.send({ url: `https://f005.backblazeb2.com/file/instadevcurso/${data.fileName}` });
+    } catch (error) {
+      return res.status(400).json({
+        error: 'Faile to upload',
+      });
+    }
   }
 }
 module.exports = new FileController();
