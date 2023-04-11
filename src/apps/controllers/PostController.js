cdf060035ee5ca951c249caf34e27845a17b1d7f
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-04-11 11:57:38 -0300

    add route to deleted post

--- src/apps/controllers/PostController.js
+++ src/apps/controllers/PostController.js
@@ -1,18 +1,55 @@
-const Post = require('../models/Post');
+const Post = require("../models/Post");
 
 class PostController {
   async store(req, res) {
     const { image, description } = req.body;
 
-    const newPost = await Post.create({ image, description, author_id: req.userId });
+    const newPost = await Post.create({
+      image,
+      description,
+      author_id: req.userId,
+    });
 
     if (!newPost) {
-      return res.status(400).json({ error: 'Created post failed!' });
+      return res.status(400).json({ error: "Created post failed!" });
     }
 
     return res.status(200).json({
-      data: image, description,
+      data: image,
+      description,
     });
+  }
+  async delete(req, res) {
+    const { id } = req.id;
+
+    const post = Post.find({
+      where: { id },
+    });
+
+    if (!post) {
+      return res.status(404).json({
+        error: "Post not exists!",
+      });
+    }
+    if (post.author_id !== req.userId) {
+      return res.status(401).json({
+        error: "You dont't have permission to delete this post!",
+      });
+    }
+
+    const deletedPost = await Post.destroy({
+      where: {
+        id,
+      },
+    });
+
+    if (!deletedPost) {
+      return res.status(400).json({
+        error: "Failed to delete this post!",
+      });
+    }
+
+    return res.status(200).json({ message: "Post deleted" });
   }
 }
 module.exports = new PostController();
--- src/routes.js
+++ src/routes.js
@@ -1,32 +1,37 @@
-const { Router } = require('express');
-const { updload } = require('./configs/multer');
+const { Router } = require("express");
+const { updload } = require("./configs/multer");
 
-const SchemaValidator = require('./apps/middlewares/SchemaValidator');
-const AuthenticationMiddleware = require('./apps/middlewares/Authentication');
+const SchemaValidator = require("./apps/middlewares/SchemaValidator");
+const AuthenticationMiddleware = require("./apps/middlewares/Authentication");
 
-const UserSchema = require('./schema/Create.User.Schema.json');
-const AuthSchema = require('./schema/Auth.Schema.json');
-const PostSchema = require('./schema/Post.Schema.json');
+const UserSchema = require("./schema/Create.User.Schema.json");
+const AuthSchema = require("./schema/Auth.Schema.json");
+const PostSchema = require("./schema/Post.Schema.json");
 
-const AuthenticationController = require('./apps/controllers/AuthenticationController');
-const UserController = require('./apps/controllers/UserController');
-const FileController = require('./apps/controllers/FileController');
-const PostController = require('./apps/controllers/PostController');
+const AuthenticationController = require("./apps/controllers/AuthenticationController");
+const UserController = require("./apps/controllers/UserController");
+const FileController = require("./apps/controllers/FileController");
+const PostController = require("./apps/controllers/PostController");
 
 const routes = new Router();
 
-routes.post('/users', SchemaValidator(UserSchema), UserController.store);
+routes.post("/users", SchemaValidator(UserSchema), UserController.store);
 
-routes.post('/auth', SchemaValidator(AuthSchema), AuthenticationController.store);
+routes.post(
+  "/auth",
+  SchemaValidator(AuthSchema),
+  AuthenticationController.store
+);
 
 routes.use(AuthenticationMiddleware);
 
-routes.put('/users', UserController.update);
-routes.delete('/users', UserController.delete);
-routes.get('/user-profile', UserController.index);
+routes.put("/users", UserController.update);
+routes.delete("/users", UserController.delete);
+routes.get("/user-profile", UserController.index);
 
-routes.post('/upload', updload.single('image'), FileController.upload);
+routes.post("/upload", updload.single("image"), FileController.upload);
 
-routes.post('/new-post', SchemaValidator(PostSchema), PostController.store);
+routes.post("/new-post", SchemaValidator(PostSchema), PostController.store);
+routes.delete("/delete-post/:id", PostController.delete);
 
 module.exports = routes;
